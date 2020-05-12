10afdca7f34a8c2010fc4e0a8b71ec94aec2672e
liu <yigelaoliu@icloud.com>
2020-05-12 14:35:17 +0800

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by liu on 2020/5/12.
+//  Copyright © 2020 laoliu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
